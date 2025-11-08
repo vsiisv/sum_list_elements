6a5fe3a6df7535c292c6ae6ed1b4bc9e3d9f0989
vsiisv <shores.freezer0u@icloud.com>
2025-11-08 19:19:50 +1000

    Update lists and improve result calculation in main.py

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-list1 = [1, 2, 3, 4, 5]
-list2 = [6, 7, 8, 9, 10]
+list1 = [1, 2, 3, 4, 5, 10, 20]
+list2 = [6, 7, 8, 9, 10, 11, 12, 13, 14]
 
 # first
-result = [a + b for a, b in zip(list1, list2)]
+result = [a + b for a, b in zip(list1, list2) if a or b is not None]
 print(result)
 
 
 # second
 def new_list(list1, list2):
     new_list = []
-    for i in range(len(list1)):
+    length = len(list1) if len(list1) < len(list2) else len(list2)
+    for i in range(length):
         new_list.append(list1[i] + list2[i])
+
     return new_list
 
 
