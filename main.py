521d8bb33866bd88cc82d468eb74582323c9b79b
vsiisv <shores.freezer0u@icloud.com>
2025-11-09 10:29:37 +1000

    changed function to account different length of lists

--- main.py
+++ main.py
@@ -1,19 +1,26 @@
-list1 = [1, 2, 3, 4, 5, 10, 20]
+list1 = [1, 2, 3, 4, 5, 10, 20, 43, 28, 19, 100, 54]
 list2 = [6, 7, 8, 9, 10, 11, 12, 13, 14]
 
+
 # first
-result = [a + b for a, b in zip(list1, list2) if a or b is not None]
-print(result)
+def new_list_sum(list1, list2):
+    difference = abs(len(list1) - len(list2))
+    list_slice = list1[-difference:] if len(list1) > len(list2) else list2[-difference:]
+    result = [a + b for a, b in zip(list1, list2) if a or b is not None]
+    return result + list_slice
 
 
 # second
 def new_list(list1, list2):
     new_list = []
+    difference = abs(len(list1) - len(list2))
+    list_slice = list1[-difference:] if len(list1) > len(list2) else list2[-difference:]
     length = len(list1) if len(list1) < len(list2) else len(list2)
     for i in range(length):
         new_list.append(list1[i] + list2[i])
 
-    return new_list
+    return new_list + list_slice
 
 
+print(new_list_sum(list1, list2))
 print(new_list(list1, list2))
