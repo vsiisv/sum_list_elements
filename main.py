67200d6a268bae881937c5e2eb315a9e35442821
vsiisv <shores.freezer0u@icloud.com>
2025-11-09 12:44:47 +1000

    corrected output for equals lists

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 list1 = [1, 2, 3, 4, 5, 10, 20, 43, 28, 19, 100, 54]
-list2 = [6, 7, 8, 9, 10, 11, 12, 13, 14]
+list2 = [8, 9, 10, 11, 12, 13, 14, 3, 5, 1]
 
 
 # first
@@ -7,7 +7,10 @@ def new_list_sum(list1, list2):
     difference = abs(len(list1) - len(list2))
     list_slice = list1[-difference:] if len(list1) > len(list2) else list2[-difference:]
     result = [a + b for a, b in zip(list1, list2) if a or b is not None]
-    return result + list_slice
+    if difference == 0:
+        return result
+    else:
+        return result + list_slice
 
 
 # second
@@ -18,8 +21,10 @@ def new_list(list1, list2):
     length = len(list1) if len(list1) < len(list2) else len(list2)
     for i in range(length):
         new_list.append(list1[i] + list2[i])
-
-    return new_list + list_slice
+    if difference == 0:
+        return new_list
+    else:
+        return new_list + list_slice
 
 
 print(new_list_sum(list1, list2))
